7e8f5c9b18646af7eabc0e9b46a72b326e741cfd
MatAndrade01 <ltbmatheus14012@gmail.com>
2025-12-08 23:01:21 -0300

    Aprendendo math e times, mm e regex

---
+++ math_e_time/mathetimes.rb
@@ -0,0 +1,19 @@
+puts Math.sqrt(16)
+
+puts Math.log10(100)
+puts radian = 30 * (Math::PI / 100)
+puts Math::E
+puts Math::PI
+
+# TIMES
+
+time = Time.now
+# puts time
+puts time.year
+puts time.month
+puts time.day
+puts time.strftime('%d/%m/%y')
+puts time.saturday?
+
+time2 = Time.now
+puts time == time2
---
+++ regex/regex.rb
@@ -0,0 +1,11 @@
+# puts /by/ =~ 'ruby'
+# puts 'ruby' =~ /by/
+# puts 'ruby' =~ /rails/
+
+# phrase = "Olá como vai você?"
+# match_data = /como/.match(phrase)
+# puts match_data
+# puts match_data.pre_match
+# puts match_data.post_match
+
+puts /\?/.match('Tudo bem?')
